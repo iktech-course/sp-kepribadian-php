11cf151ec03703f9ce0c56d660a5dbf730245f78
andestroy15 <andestiintan5@gmail.com>
2023-06-17 11:01:12 +0700

    Initializing Project

---
+++ config/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+ 
+$databaseHost = 'localhost';
+$databaseName = 'sp_kepribadian';
+$databaseUsername = 'root';
+$databasePassword = '';
+ 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
